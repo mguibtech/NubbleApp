768d5f5afe8b4415daf9dc238ac3fc5599fd88ce
Marcos Guibson <mguibtech@gmail.com>
2023-12-13 05:31:44 -0400

    Adding basic component textInput

--- App.tsx
+++ App.tsx
@@ -15,18 +15,34 @@ import {Box} from './src/components/Box/Box';
 import {EyeOffIcon} from './src/assets/icons/EyeOffIcon';
 import {EyeOnIcon} from './src/assets/icons/EyeOnIcon';
 import {Icon} from './src/components/Icon/Icon';
+import {TextInput} from './src/components/TextInput/TextInput';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
-        <Text preset="paragraphLarge" italic bold>
-          Testando
-        </Text>
-        <Text preset="headingLarge">Fonte Padrao</Text>
-        <Icon name="eyeOn" color="errorLight" size={40} />
-        <Icon name="camera" color="errorLight" size={40} />
-        <Icon name="message" color="errorLight" size={40} />
+        <View style={{paddingHorizontal: 24}}>
+          <Text preset="headingLarge" marginBottom="s8">
+            Olá!
+          </Text>
+          <Text preset="paragraphLarge" mb="s40">
+            Digite seu e-mail e senha para entrar
+          </Text>
+
+          <Box mb="s20">
+            <TextInput label="E-mail" placeholder="Digite seu e-mail" />
+          </Box>
+          <Box>
+            <TextInput label="Senha" placeholder="Digite sua senha" />
+          </Box>
+
+          <Text mt="s10" preset="paragraphSmall" bold color="primary">
+            Esquci minha senha
+          </Text>
+
+          <Button title="Entrar" mt="s48" />
+          <Button title="Criar uma conta" preset="outline" mt="s12" />
+        </View>
       </SafeAreaView>
     </ThemeProvider>
   );
