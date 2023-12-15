b3b0e55252245bcb452605dfceb8eaf668783a41
Marcos Guibson <mguibtech@gmail.com>
2023-12-14 22:19:49 -0400

    Finishing Screen component

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -3,25 +3,45 @@ import {Box} from '../Box/Box';
 import {useAppSafeArea} from '../Hooks/useAppSafeArea';
 import {Icon} from '../Icon/Icon';
 import {Text} from '../Text/Text';
+import {KeyboardAvoidingView, Platform, ScrollView} from 'react-native';
+import {ScrollViewContainer, ViewContainer} from './components/ScreenContainer';
+import {useAppTheme} from '../Hooks/useAppTheme';
 
 interface ScreenProps {
   children: React.ReactNode;
   canGoBack?: boolean;
+  scrollable?: boolean;
 }
 
-export function Screen({children, canGoBack = false}: ScreenProps) {
-  const {top} = useAppSafeArea();
+export function Screen({
+  children,
+  canGoBack = false,
+  scrollable = false,
+}: ScreenProps) {
+  const {top, bottom} = useAppSafeArea();
+
+  const Container = scrollable ? ScrollViewContainer : ViewContainer;
+  const {colors} = useAppTheme();
+
   return (
-    <Box paddingHorizontal="s24">
-      {canGoBack && (
-        <Box mb="s24" flexDirection="row">
-          <Icon name="arrowLeft" color="primary" />
-          <Text preset="paragraphMedium" semiBold ml="s8">
-            Voltar
-          </Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <Container backgroundColor={colors.background}>
+        <Box
+          paddingBottom="s24"
+          paddingHorizontal="s24"
+          style={{paddingTop: top, paddingBottom: bottom}}>
+          {canGoBack && (
+            <Box mb="s24" flexDirection="row">
+              <Icon name="arrowLeft" color="primary" />
+              <Text preset="paragraphMedium" semiBold ml="s8">
+                Voltar
+              </Text>
+            </Box>
+          )}
+          {children}
         </Box>
-      )}
-      {children}
-    </Box>
+      </Container>
+    </KeyboardAvoidingView>
   );
 }
--- src/screens/auth/SignUpScreen/SignUpScreen.tsx
+++ src/screens/auth/SignUpScreen/SignUpScreen.tsx
@@ -3,13 +3,14 @@ import {Screen} from '../../../components/Screen/Screen';
 import {Text} from '../../../components/Text/Text';
 import {TextInput} from '../../../components/TextInput/TextInput';
 import {Button} from '../../../components/Button/Button';
+import {Icon} from '../../../components/Icon/Icon';
 
 export function SignUpScreen() {
   function submitForm() {
     // TODO: implementar
   }
   return (
-    <Screen canGoBack>
+    <Screen canGoBack scrollable>
       <Text preset="headingLarge" mb="s32">
         Criar uma conta
       </Text>
@@ -26,12 +27,24 @@ export function SignUpScreen() {
         boxProps={{mb: 's20'}}
       />
 
-      {/* <PasswordInput
+      <TextInput
         label="Senha"
-        secureTextEntry
-        placeholder="Digite sua senha"
+        placeholder="Digite a senha"
+        RightComponent={<Icon color="gray2" name="eyeOn" />}
         boxProps={{mb: 's48'}}
-      /> */}
+      />
+      <TextInput
+        label="Senha"
+        placeholder="Digite a senha"
+        RightComponent={<Icon color="gray2" name="eyeOn" />}
+        boxProps={{mb: 's48'}}
+      />
+      <TextInput
+        label="Senha"
+        placeholder="Digite a senha"
+        RightComponent={<Icon color="gray2" name="eyeOn" />}
+        boxProps={{mb: 's48'}}
+      />
 
       <Button onPress={submitForm} title="Criar uma conta" />
     </Screen>
