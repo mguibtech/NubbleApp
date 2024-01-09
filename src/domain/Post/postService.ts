11b0a99d565a266c351542a20c25f494c12d3fb3
Marcos Guibson <mguibtech@gmail.com>
2024-01-09 19:05:33 -0400

    Adding logic of scroll infinity

--- src/domain/Post/postService.ts
+++ src/domain/Post/postService.ts
@@ -1,14 +1,19 @@
+import {Page} from '@types';
 import {postAdapter} from './postAdapter';
 import {postApi} from './postApi';
 import {Post} from './postTypes';
+import {apiAdapter} from '@api';
 
-async function getList(page: number): Promise<Post[]> {
+async function getList(page: number): Promise<Page<Post>> {
   const postPageAPI = await postApi.getList({page, per_page: 5});
 
   // throw new Error('Erro de teste');
   // return [];
 
-  return postPageAPI.data.map(postAdapter.toPost);
+  return {
+    data: postPageAPI.data.map(postAdapter.toPost),
+    meta: apiAdapter.toMetaDataPage(postPageAPI.meta),
+  };
 }
 
 export const postService = {
--- src/domain/Post/useCases/usepostList.ts
+++ src/domain/Post/useCases/usepostList.ts
@@ -6,15 +6,21 @@ export function usePostList() {
   const [error, setError] = useState<boolean | null>(null);
   const [postList, setPostList] = useState<Post[]>([]);
   const [page, setPage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(true);
 
   async function fetchInitialData() {
     try {
       setError(null);
       setLoading(true);
-      const list = await postService.getList(1);
+      const {data, meta} = await postService.getList(1);
       //TODO: VALIDAR ISSO DDEPOIS SE TEM MAIS PAGINA
-      setPage(2);
-      setPostList(list);
+      setPostList(data);
+      if (meta.hasNextPage) {
+        setPage(2);
+        setHasNextPage(true);
+      } else {
+        setHasNextPage(false);
+      }
     } catch (er) {
       console.log('ERROR? =>>>> ' + error);
       setError(true);
@@ -24,12 +30,18 @@ export function usePostList() {
   }
 
   async function fetchNexPage() {
-    if (loading) return;
+    if (loading || !hasNextPage) {
+      return;
+    }
     try {
       setLoading(true);
-      const list = await postService.getList(page);
-      setPostList(prev => [...prev, ...list]);
-      setPage(prev => prev + 1);
+      const {data, meta} = await postService.getList(page);
+      setPostList(prev => [...prev, ...data]);
+      if (meta.hasNextPage) {
+        setPage(prev => prev + 1);
+      } else {
+        setHasNextPage(false);
+      }
     } catch (err) {
       setError(true);
     } finally {
